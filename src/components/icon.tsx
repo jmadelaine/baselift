d4efeaa1b00b0aad0463d126fe1e6176c8cf6a44
JonathanMadelaine <jmadelaine91@gmail.com>
2020-04-04 09:07:24 +1300

    Allow string or string array for Icon path and pathFill props

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -3,33 +3,39 @@ import { jsx } from '@emotion/core'
 import { forwardRef, SVGAttributes } from 'react'
 
 export interface IconProps {
-  colors?: string[]
-  paths?: string[]
+  path?: string | string[]
+  pathFill?: string | string[]
 }
 
 export const Icon = forwardRef<SVGSVGElement, IconProps & SVGAttributes<SVGSVGElement>>(
-  ({ colors = [], paths = [], ...other }, ref) => (
-    <svg
-      ref={ref}
-      css={{
-        color: 'inherit',
-        width: '24px',
-        height: '24px',
-        userSelect: 'none',
-        display: 'inline-block',
-        fill: colors.length ? 'none' : 'currentColor',
-        flexShrink: 0,
-      }}
-      focusable="false"
-      viewBox="0 0 24 24"
-      {...other}
-    >
-      {paths.map((p, i) => (
-        <path
-          key={i}
-          fill={colors.length > i ? colors[i] : colors.length ? colors[colors.length - 1] : 'inherit'}
-          d={p}
-        />
-      ))}
-    </svg>
-  )
+  ({ path, pathFill, ...other }, ref) => {
+    const pathFills = typeof pathFill === 'object' ? pathFill : typeof pathFill === 'string' ? [pathColor] : []
+    const paths = typeof path === 'object' ? path : typeof path === 'string' ? [path] : []
+
+    return (
+      <svg
+        ref={ref}
+        css={{
+          color: 'inherit',
+          width: '24px',
+          height: '24px',
+          userSelect: 'none',
+          display: 'inline-block',
+          fill: pathFills.length ? 'none' : 'currentColor',
+          flexShrink: 0,
+        }}
+        focusable="false"
+        viewBox="0 0 24 24"
+        {...other}
+      >
+        {paths.map((p, i) => (
+          <path
+            key={i}
+            fill={pathFills.length > i ? pathFills[i] : pathFills.length ? pathFills[pathFills.length - 1] : 'inherit'}
+            d={p}
+          />
+        ))}
+      </svg>
+    )
+  }
+)
