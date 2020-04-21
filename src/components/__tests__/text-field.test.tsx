bef21d19f36d4b4fe10b9c404ef6882a613fbb93
JonathanMadelaine <jmadelaine91@gmail.com>
2020-04-21 15:51:52 +1200

    Add Text Field component

---
+++ src/components/__tests__/text-field.test.tsx
@@ -0,0 +1,87 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { ReactElement } from 'react'
+import { create, ReactTestRendererJSON } from 'react-test-renderer'
+import { matchers } from 'jest-emotion'
+import { TextField } from '../text-field'
+
+expect.extend(matchers)
+
+const render = (el: ReactElement) => create(el).toJSON() || ({} as ReactTestRendererJSON)
+
+describe('Text Field', () => {
+  it('renders', () => {
+    const { type } = render(<TextField />)
+
+    expect(type).toBe('input')
+  })
+  it('has default props', () => {
+    const { props } = render(<TextField />)
+
+    expect(props).toMatchObject({
+      type: 'text',
+    })
+  })
+  it('accepts basic html props', () => {
+    const { props } = render(<TextField id="someId" />)
+
+    expect(props).toMatchObject({
+      id: 'someId',
+    })
+  })
+  it('allows default props to be overwritten', () => {
+    const { props } = render(<TextField type="password" />)
+
+    expect(props).not.toMatchObject({
+      type: 'text',
+    })
+    expect(props).toMatchObject({
+      type: 'password',
+    })
+  })
+  it('has default style', () => {
+    const res = render(<TextField />)
+
+    expect(res).toHaveStyleRule('appearance', 'none')
+    expect(res).toHaveStyleRule('background', '0')
+    expect(res).toHaveStyleRule('border', '0')
+    expect(res).toHaveStyleRule('box-sizing', 'border-box')
+    expect(res).toHaveStyleRule('color', 'inherit')
+    expect(res).toHaveStyleRule('display', 'inline-block')
+    expect(res).toHaveStyleRule('font-family', 'inherit')
+    expect(res).toHaveStyleRule('font-size', '1rem')
+    expect(res).toHaveStyleRule('line-height', 'inherit')
+    expect(res).toHaveStyleRule('margin', '0')
+    expect(res).toHaveStyleRule('padding', '0')
+    expect(res).toHaveStyleRule('position', 'relative')
+    expect(res).toHaveStyleRule('vertical-align', 'middle')
+    expect(res).toHaveStyleRule('-webkit-tap-highlight-color', 'transparent')
+    expect(res).toHaveStyleRule('color', 'inherit', { target: '::placeholder' })
+    expect(res).toHaveStyleRule('opacity', '0.5', { target: '::placeholder' })
+    expect(res).toHaveStyleRule('cursor', 'not-allowed', { target: ':disabled' })
+    expect(res).toHaveStyleRule('z-index', '1', { target: ':focus' })
+  })
+  it('accepts css prop', () => {
+    const res = render(
+      <TextField
+        css={{
+          color: 'hotpink',
+        }}
+      />
+    )
+
+    expect(res).toHaveStyleRule('color', 'hotpink')
+  })
+  it('allows default style to be overwritten', () => {
+    const res = render(
+      <TextField
+        css={{
+          position: 'absolute',
+        }}
+      />
+    )
+
+    expect(res).not.toHaveStyleRule('position', 'relative')
+    expect(res).toHaveStyleRule('position', 'absolute')
+  })
+})
---
+++ src/components/text-field.tsx
@@ -0,0 +1,46 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { forwardRef, InputHTMLAttributes } from 'react'
+
+const validTextFieldTypes = ['email', 'hidden', 'password', 'tel', 'text', 'url'] as const
+
+interface TextFieldProps {
+  type?: typeof validTextFieldTypes[number]
+}
+
+export const TextField = forwardRef<
+  HTMLInputElement,
+  TextFieldProps & Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>
+>(({ type, ...rest }, ref) => (
+  <input
+    ref={ref}
+    css={{
+      appearance: 'none',
+      background: 0,
+      border: 0,
+      boxSizing: 'border-box',
+      color: 'inherit',
+      display: 'inline-block',
+      fontFamily: 'inherit',
+      fontSize: '1rem',
+      lineHeight: 'inherit',
+      margin: 0,
+      padding: 0,
+      position: 'relative',
+      verticalAlign: 'middle',
+      WebkitTapHighlightColor: 'transparent',
+      '::placeholder': {
+        color: 'inherit',
+        opacity: '0.5',
+      },
+      '&:disabled': {
+        cursor: 'not-allowed',
+      },
+      '&:focus': {
+        zIndex: 1,
+      },
+    }}
+    type={type && validTextFieldTypes.indexOf(type) > -1 ? type : 'text'}
+    {...rest}
+  />
+))
