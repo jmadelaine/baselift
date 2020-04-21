828bb9db4645676822d2428c4ce1a123170a96e3
JonathanMadelaine <jmadelaine91@gmail.com>
2020-04-21 15:51:09 +1200

    Restrict Button type prop to valid button input types, and update base button style

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,33 +2,46 @@
 import { jsx } from '@emotion/core'
 import { forwardRef, ButtonHTMLAttributes } from 'react'
 
-export const Button = forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElement>>(
-  (props, ref) => (
-    <button
-      ref={ref}
-      css={{
-        appearance: 'none',
-        background: 'none',
-        border: 0,
-        boxSizing: 'border-box',
-        cursor: 'pointer',
-        display: 'inline-block',
-        outline: 0,
-        position: 'relative',
-        textOverflow: 'ellipsis',
-        userSelect: 'none',
-        WebkitTouchCallout: 'none',
-        verticalAlign: 'middle',
-        whiteSpace: 'nowrap',
-        '&:disabled': {
-          cursor: 'not-allowed',
-        },
-        '&:focus': {
-          zIndex: 1,
-        },
-      }}
-      type="button"
-      {...props}
-    />
-  )
-)
+const validButtonTypes = ['button', 'reset', 'submit'] as const
+
+interface ButtonProps {
+  type?: typeof validButtonTypes[number]
+}
+
+export const Button = forwardRef<
+  HTMLButtonElement,
+  ButtonProps & Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type'>
+>(({ type, ...rest }, ref) => (
+  <button
+    ref={ref}
+    css={{
+      appearance: 'none',
+      background: 0,
+      border: 0,
+      boxSizing: 'border-box',
+      color: 'inherit',
+      cursor: 'pointer',
+      display: 'inline-block',
+      fontFamily: 'inherit',
+      fontSize: '1rem',
+      lineHeight: 'inherit',
+      margin: 0,
+      padding: 0,
+      position: 'relative',
+      textOverflow: 'ellipsis',
+      userSelect: 'none',
+      verticalAlign: 'middle',
+      WebkitTapHighlightColor: 'transparent',
+      WebkitTouchCallout: 'none',
+      whiteSpace: 'nowrap',
+      '&:disabled': {
+        cursor: 'not-allowed',
+      },
+      '&:focus': {
+        zIndex: 1,
+      },
+    }}
+    type={type && validButtonTypes.indexOf(type) > -1 ? type : 'button'}
+    {...rest}
+  />
+))
